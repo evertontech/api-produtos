924e69881c08e869e367014c32991c6d401cb515
Everton Santos <everton_projeto@outlook.com>
2024-07-25 13:52:14 -0300

    Initial commit

---
+++ src/test/java/io/github/evertontech/api_produtos/ApiProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.evertontech.api_produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
